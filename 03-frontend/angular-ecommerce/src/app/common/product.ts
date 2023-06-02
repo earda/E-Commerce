4f97dcdd0b9b2694a3109b1c047ec62982a0a228
Erdem <earda230@gmail.com>
2023-06-02 15:36:18 +0300

    Run the App with Error
    Run the App with Error

--- 03-frontend/angular-ecommerce/src/app/common/product.ts
+++ 03-frontend/angular-ecommerce/src/app/common/product.ts
@@ -1,6 +1,7 @@
 export class Product {
 
-    constructor(public sku: string,
+    constructor(
+        public sku: string,
         public name: string,
         public description: string,
         public unitPrice: number,
