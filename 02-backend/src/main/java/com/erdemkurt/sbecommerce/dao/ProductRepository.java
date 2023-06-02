d65a0e5d543cca70513381db0d98a502d804821e
Erdem <earda230@gmail.com>
2023-06-02 12:05:13 +0300

    Listed Products and edited their name from DB
    Listed Products and edited their name from DB

--- 02-backend/src/main/java/com/erdemkurt/sbecommerce/dao/ProductRepository.java
+++ 02-backend/src/main/java/com/erdemkurt/sbecommerce/dao/ProductRepository.java
@@ -2,7 +2,9 @@ package com.erdemkurt.sbecommerce.dao;
 
 import com.erdemkurt.sbecommerce.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product,Long> {
 
 }
