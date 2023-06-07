e7b9867391eaaf67d124641e4dd0040b6bb1694e
Erdem <earda230@gmail.com>
2023-06-07 10:20:08 +0300

    fixed list
    fixed list

--- 03-frontend/angular-ecommerce/src/app/services/product.service.ts
+++ 03-frontend/angular-ecommerce/src/app/services/product.service.ts
@@ -9,7 +9,7 @@ import {map} from 'rxjs/operators';
 })
 export class ProductService {
 
-  private baseUrl = 'http://localhost:8080/api/products?size=100';
+  private baseUrl = 'http://localhost:8080/api/products';
 
   constructor(private httpClient:HttpClient) { }
 
