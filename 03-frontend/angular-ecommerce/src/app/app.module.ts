b5cc530fa1b0d637e589ceb80a633424f3a1b280
Erdem <earda230@gmail.com>
2023-06-11 17:43:33 +0300

    Display a message if products not found
    Display a message if products not found

--- 03-frontend/angular-ecommerce/src/app/app.module.ts
+++ 03-frontend/angular-ecommerce/src/app/app.module.ts
@@ -13,13 +13,13 @@ import{Routes,RouterModule} from '@angular/router';
 import { ProductCategoryMenuComponent } from './components/product-category-menu/product-category-menu.component';
 import { SearchComponent } from './components/search/search.component';
 const routes: Routes = [
+  {path:'search/:keyword', component: ProductListComponent},
  {path:'category/:id',component:ProductListComponent},
  {path: 'category/:id/:name', component: ProductListComponent},
  {path:'category',component:ProductListComponent},
  {path:'products',component:ProductListComponent},
  {path:'',redirectTo:'/products',pathMatch:'full'},
  {path:'**',redirectTo:'/products',pathMatch:'full'},
- {path:'search/:keyword', component: ProductListComponent},
 ];
 
 @NgModule({
